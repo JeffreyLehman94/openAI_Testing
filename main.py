cda0b8a4af8b5762d5d810cd982f45232e95e3c4
Jeffrey Lehman <JeffreyLehman94@gmail.com>
2023-03-03 15:01:54 -0500

    Switched language model. Removed selenium.

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random
 import praw
-from gpt_test import request_gpt
 import time
 from openMethods import *
+
 
 def joinSubs():
     with open('subreddits.txt') as file:
@@ -20,6 +20,7 @@ def checkShadowBan(username):
     except Exception as e:
         print(str(e) + '\n Probably shadow banned!')
 
+
 def getSubKeywords(debug):
     SUBREDDITS = list(SUBREDDITS_KEYWORDS.keys())
     SELECTED = SUBREDDITS[random.randint(0, len(SUBREDDITS) - 1)]
@@ -33,6 +34,7 @@ def main(debug):
     SUBREDDIT_KEYWORDS = getSubKeywords(0)
     subreddit = reddit.subreddit(SUBREDDIT_KEYWORDS[0])
     search_query = SUBREDDIT_KEYWORDS[1]
+    checkShadowBan(CLIENT_USERNAME[0])
     if debug == 1:
         print(CLIENT_ID[0] + '\n')
         print(CLIENT_SECRET[0] + '\n')
@@ -48,20 +50,12 @@ def main(debug):
                         any(keyword in comment.body.lower() for keyword in search_query) and \
                         not any(stop_word in comment.body.lower() for stop_word in STOP_WORDS[0]) and \
                         len(comment.body.lower()) > 30:
-                    try:
-                        reply_text = request_gpt(comment.body)
-                        print('POSTING COMMENT:%s' % reply_text)
-                        print('YOU HAVE 10 SECONDS TO STOP THIS POST IF YOU WANT.')
-                    except Exception as e:
-                        print(str(e) + '\n GPT EXCEPTION')
-                        break;
-                    time.sleep(10)
-                    try:
-                        comment.reply(reply_text)
-                        print("COMMENT MADE\n")
-                    except Exception as e:
-                        print(str(e) + '\n POSTING EXCEPTION')
-                        break;
+                    reply_text = getResponse(comment.body, CLIENT_USERNAME[0])
+                    print('POSTING COMMENT:%s' % reply_text)
+                    print('YOU HAVE 10 SECONDS TO STOP THIS POST IF YOU WANT.')
+                    time.sleep(1000000)
+                    comment.reply(reply_text)
+                    print("COMMENT MADE\n")
                     comments_in_thread += 1
                     time.sleep(random.randint(60 * 5, 60 * 10))
 
@@ -73,3 +67,5 @@ reddit = praw.Reddit(
     password=CLIENT_PASSWORD[0],
     user_agent=CLIENT_USER_AGENT[0]
 )
+
+main(0)
--- openMethods.py
+++ openMethods.py
@@ -8,34 +8,31 @@ from datetime import date
 
 def getResponse(prompt, user):
     openai.api_key = OPENAI_KEY[0]
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=prompt,
-        temperature=0.7,
-        max_tokens=10,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": "Create a response to this reddit comment. Do not mention you are an AI in anyway. Answer it as if you are a 85 year old person:%s" % prompt},
+        ])
     print(response)
     response_data = extractResponse(json.dumps(response))
     saveResponse(CLIENT_USERNAME[0], prompt, response_data[0], response_data[2], response_data[1])
-    return response_data
+    return response_data[0]
 
 
 def extractResponse(json_data):
     data = json.loads(json_data)
     model = data['model']
-    response_text = data['choices'][0]['text']
+    response_text = data['choices'][0]['message']['content']
     total_tokens = data['usage']['total_tokens']
     return response_text, model, total_tokens
+
 
 def saveResponse(user, prompt, response, tokens, model):
     path = 'db/db.sqlite3'
     scriptdir = os.path.dirname(__file__)
     db_path = os.path.join(scriptdir, path)
     os.makedirs(os.path.dirname(db_path), exist_ok=True)
-    con = sqlite3.connect(db_path+"responses.db")
+    con = sqlite3.connect(db_path + "responses.db")
     cur = con.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS REQUESTS(DATE, USER, PROMPT, RESPONSE, TOKENS, MODEL)")
     cur.execute(" INSERT INTO REQUESTS VALUES(?, ?, ?,?, ?, ?)",
